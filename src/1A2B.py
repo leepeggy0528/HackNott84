6f12c76fc55afbd1c44b4194ea140f2524b8efd4
leepeggy0528 <67175663+leepeggy0528@users.noreply.github.com>
2023-11-05 12:05:25 +0000

    m

--- src/1A2B.py
+++ src/1A2B.py
@@ -3,20 +3,24 @@ import random
 def analysis(ans,guess):
     A=0;
     B=0;
+    print(ans)
     for i in range(4):
+         print("A=",A,"B=",B)
+         print(ans[i],guess[i])
          if guess[i]==ans[i]:
-              A+=1
+            A+=1
          else:
-              if guess[i] in ans:
-                   B+=1;
+            if guess[i] in set(ans):
+                B+=1;
     return A,B
 
 nums=[0,1,2,3,4,5,6,7,8,9]
 ans=random.sample(nums,4);
 
-while 1>True:
+while 1>0:
     guess= input("Please enter a 4-digit number:")
-    A=B=analysis(ans,guess)
+    A=analysis(ans,guess)[0]
+    B=analysis(ans,guess)[1]
     print(analysis(ans,guess))
     if A==4:
         print("Congratulation!!!")
